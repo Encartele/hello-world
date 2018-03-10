0928d8282d648db7a5303eca5438e1a5abd290a1
emphassis <emphassis@gmail.com>
2018-03-10 00:09:34 -0600

    Testing commit

---
+++ app/src/main/java/net/cidnet/customer/helloworld/MainActivity.java
@@ -0,0 +1,29 @@
+package net.cidnet.customer.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+   public TextView hwtv;
+   public Button hwbtn;
+   public int i;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        hwtv = findViewById(R.id.hwTV);
+        hwbtn = findViewById(R.id.hwBtn);
+        hwbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                i++;
+                hwtv.setText("You have pressed the Hello World button " +  i + " time(s)!");
+            }
+        });
+    }
+}
